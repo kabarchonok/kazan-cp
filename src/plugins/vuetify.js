a4ee58ce491c0a98649cb30cda0e51c1ed83a84f
VanyaErohin <bulahoff7@gmail.com>
2019-09-28 16:33:49 +0500

    Updated theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,4 +7,14 @@ export default new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
+  theme: {
+    themes: {
+      light: {
+        primary: '#2da374',
+        secondary: '#b0bec5',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+      },
+    },
+  },
 });
