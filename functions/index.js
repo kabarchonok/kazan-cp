d2552514750a8c7dad6fa78c8fdb1a8f9e0d521a
Daniil <me@dan1.ru>
2019-09-28 19:30:32 +0500

    tg notification and post endpoint

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const telegraf = require('telegraf');
 admin.initializeApp();
 const firestore = admin.firestore();
 
@@ -9,4 +10,15 @@ const firestore = admin.firestore();
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
+});
+
+const CHAT_ID = 426125308;
+
+
+exports.sendNotifications = functions.auth.user().onCreate(_ => {
+	const bot = new telegraf.Telegram(functions.config().bot.token);
+	return bot.sendMessage(
+		CHAT_ID,
+		'Новое мероприятие'
+	);
 });
